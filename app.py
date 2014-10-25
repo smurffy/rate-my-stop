c3deb855b43b3c4c15b71b026c5db165efdd8a40
Dan Getelman <dgetelman@gmail.com>
2014-10-25 15:48:25 -0700

    Have two stages of texting

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 import twilio.twiml
  
 app = Flask(__name__)
+app.SECRET_KEY = 'notsecret'  # TODO: put securely on Heroku
  
 @app.route("/twilio_sms", methods=['GET', 'POST'])
-def hello_monkey():
+def incoming_sms():
     """Respond to incoming calls with a simple text message."""
- 
-    body = request.values.get('Body')
+    
+    if 'stop_id' not in session:
+        # if it's the first text...
 
-    resp = twilio.twiml.Response()
-    resp.message("Hello, " + body)
-    return str(resp)
+        # TODO: validate that the stop_id is a stop id
+        session['stop_id'] = request.values.get('Body')
+
+        resp = twilio.twiml.Response()
+        resp.message("Tell us your feedback:")
+        return str(resp)
+    else: 
+        # if it's the 2nd text...
+
+        feedback = request.values.get('Body')
+
+        # TODO: save data here
+        resp = twilio.twiml.Response()
+        resp.message("Thanks for your feedback!")
+        return str(resp)
  
 if __name__ == "__main__":
     app.run(debug=True)
