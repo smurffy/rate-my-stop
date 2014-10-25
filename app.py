689f67b1c922e7b7099978c8c1f3c325daa97692
Dan Getelman <dgetelman@gmail.com>
2014-10-25 15:11:06 -0700

    Try sending body back

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import twilio.twiml
  
 app = Flask(__name__)
  
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/twilio_sms", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
  
+    body = request.values.get('Body')
+
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message("Hello, " + body)
     return str(resp)
  
 if __name__ == "__main__":
